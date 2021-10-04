49f38498d560a3846cbce276b673749803536e0f
Sebastián Rodríguez Galarza <42384931+SebasRod23@users.noreply.github.com>
2021-10-04 00:58:06 -0500

    Chi-Square validator (#11)
    * Create Chi-square validator
    
    * Implement validate and getData functions
    
    * Refact code into the corresponding interfaces and classes
    
    * Fix bugs and make validate function async

---
+++ src/Interfaces/ChiSquareData.ts
@@ -0,0 +1,17 @@
+export interface ChiSquareData {
+  range: number;
+  k: number;
+  classes: number;
+  table: ChiSquareCell[];
+  X0: number;
+  X1: number;
+}
+
+export interface ChiSquareCell {
+  start: number;
+  end: number;
+  absolute: number;
+  probability: number;
+  theoretical: number;
+  result: number;
+}
---
+++ src/classes/ChiSquare.ts
@@ -0,0 +1,87 @@
+import { error } from 'console';
+import { ChiSquareData, ChiSquareCell } from '../Interfaces/ChiSquareData';
+import { Validator } from '../Interfaces/Validator';
+
+export class ChiSquare implements Validator {
+  public range!: number;
+  public k!: number;
+  public classes!: number;
+  public table!: ChiSquareCell[];
+  public X0!: number;
+  public X1!: number;
+
+  public validate = async (
+    randoms: number[],
+    alpha: number,
+  ): Promise<boolean> => {
+    if (randoms.length <= 4) {
+      return Promise.reject(
+        'Not enough information provided to make the validation',
+      );
+    }
+    this.range = randoms[randoms.length - 1] - randoms[0];
+    this.k = Math.floor(1 + 3.322 * Math.log10(randoms.length));
+    this.classes = this.range / this.k;
+    this.createTable(randoms);
+    this.getTheoreticalValue(alpha);
+    return this.X0 < this.X1;
+  };
+
+  public getData = (): ChiSquareData => {
+    const data: ChiSquareData = {
+      range: this.range,
+      k: this.k,
+      classes: this.classes,
+      table: this.table,
+      X0: this.X0,
+      X1: this.X1,
+    };
+    return data;
+  };
+
+  private createTable = (randoms: number[]) => {
+    let i = 0;
+    let j = 0;
+    this.table = [];
+    this.X0 = 0;
+    while (i < this.k) {
+      let start = i * this.classes;
+      let end = (i + 1) * this.classes;
+      let absolute = 0;
+      while (randoms[j] <= end) {
+        absolute++;
+        j++;
+        if (j < randoms.length && randoms[j] > end && absolute < 5) {
+          i++;
+          end = (i + 1) * this.classes;
+        }
+      }
+      if (absolute < 5 && i >= this.k - 1) {
+        const last = this.table.pop();
+        start = last!.start;
+        absolute += last!.absolute;
+      }
+      const probability = end - start;
+      const theoretical = randoms.length * probability;
+      const result = Math.pow(absolute - theoretical, 2) / theoretical;
+      const cell: ChiSquareCell = {
+        start: start,
+        end: end,
+        absolute: absolute,
+        probability: probability,
+        theoretical: theoretical,
+        result: result,
+      };
+      this.table.push(cell);
+      this.X0 += cell.result;
+      i++;
+    }
+    this.k = this.table.length;
+  };
+
+  private getTheoreticalValue = (alpha: number) => {
+    const v = this.k - 1;
+    //TODO: Get value from tables with v and alpha
+    this.X1 = Number.MAX_SAFE_INTEGER;
+  };
+}
