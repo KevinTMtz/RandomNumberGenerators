75adb10cfa7a674645e21e7da534fab80fa2da0d
KevinTMtz <torresmtzkevin@gmail.com>
2021-10-07 16:19:37 -0500

    Input form with validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-/** @jsxImportSource @emotion/react */
 import React from 'react';
-import { css } from '@emotion/react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
-
-const rootDivStyle = css({
-  margin: '0px 24px',
-  '@media (max-width: 600px)': {
-    margin: '0px 16px',
-  },
-});
+import Layout from './containers/Layout';
 
 const App = () => (
   <div className='App'>
-    <header>
-      <AppBar position='static'>
-        <Toolbar variant='dense'>
-          <Typography variant='h6' color='inherit' component='div'>
-            Random Number Generator
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </header>
-    <div css={rootDivStyle}>
-      <h1>Page content</h1>
-    </div>
+    <Layout />
   </div>
 );
 
---
+++ src/enums/RNGOptions.ts
@@ -0,0 +1,7 @@
+export enum RNGOptions {
+  MiddleSquares = '1',
+  LinearCongruential = '2',
+  MixedCongruential = '3',
+  CombinedCongruential = '4',
+  MultiplicativeCongruential = '5',
+}
